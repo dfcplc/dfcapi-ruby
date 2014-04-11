04bc761f43e6017d61946e3f4e4dbd56d1583f8d
Matt Clements <matt@mattclements.co.uk>
2014-04-11 22:06:11 +0100

    Removing Nested Classes

--- lib/dfcapi.rb
+++ lib/dfcapi.rb
@@ -40,269 +40,266 @@ module Dfcapi
   @@raw_body = false
   @@headers = {}
 
-  class Dfcapi
+  def self.setCheckKeyUrl(checkkey_url)
+    @@api_url_checkkey = checkkey_url
+  end
 
-    def self.setCheckKeyUrl(checkkey_url)
-      @@api_url_checkkey = checkkey_url
+  def self.setViewDirectDebitUrl(viewdd_url)
+      @@api_url_viewdd = viewdd_url
+  end
+
+  def self.setViewDirectDebitBreakdownUrl(viewddbreakdown_url)
+      @@api_url_viewddbreakdown = viewddbreakdown_url
+  end
+
+  def self.setCreateDirectDebitUrl(setupdd_url)
+      @@api_url_setupdd = setupdd_url
+  end
+
+  def self.setUpdateDirectDebitUrl(updatedd_url)
+      @@api_url_updatedd = updatedd_url
+  end
+
+  def self.setCancelDirectDebitUrl(canceldd_url)
+      @@api_url_canceldd = canceldd_url
+  end
+
+  def self.clearStoredResponse
+      @@errors = {}
+      @@response_code = false
+      @@body = false
+      @@raw_body = false
+      @@headers = {}
+  end
+
+  def self.getErrors
+      return @@errors
+  end
+
+  def self.getResponseCode
+      return @@response_code
+  end
+
+  def self.getResponseBody
+      return @@body
+  end
+
+  def self.getResponseBodyRaw
+      return @@raw_body
+  end
+
+  def self.getResponseHeaders
+      return @@headers
+  end
+
+  def self.setStoredResponse(response)
+      @@errors = response.body['errors']
+      @@response_code = response.code
+      @@body = response.body
+      @@raw_body = response.raw_body
+      @@headers = response.headers
+  end
+
+  def self.checkApiKey(api_key, api_secret)
+    self.clearStoredResponse
+
+    response = Unirest.get @@api_url_checkkey, auth:{:user=>api_key, :password=>api_secret}
+
+    self.setStoredResponse response
+
+    if(response.code === 200 && response.body['status'] === 'ok')
+      return true
     end
 
-    def self.setViewDirectDebitUrl(viewdd_url)
-        @@api_url_viewdd = viewdd_url
-    end
 
-    def self.setViewDirectDebitBreakdownUrl(viewddbreakdown_url)
-        @@api_url_viewddbreakdown = viewddbreakdown_url
-    end
+    return false
 
-    def self.setCreateDirectDebitUrl(setupdd_url)
-        @@api_url_setupdd = setupdd_url
-    end
+  end
 
-    def self.setUpdateDirectDebitUrl(updatedd_url)
-        @@api_url_updatedd = updatedd_url
-    end
-
-    def self.setCancelDirectDebitUrl(canceldd_url)
-        @@api_url_canceldd = canceldd_url
-    end
-
-    def self.clearStoredResponse
-        @@errors = {}
-        @@response_code = false
-        @@body = false
-        @@raw_body = false
-        @@headers = {}
-    end
-
-    def self.getErrors
-        return @@errors
-    end
-
-    def self.getResponseCode
-        return @@response_code
-    end
-
-    def self.getResponseBody
-        return @@body
-    end
-
-    def self.getResponseBodyRaw
-        return @@raw_body
-    end
-
-    def self.getResponseHeaders
-        return @@headers
-    end
-
-    def self.setStoredResponse(response)
-        @@errors = response.body['errors']
-        @@response_code = response.code
-        @@body = response.body
-        @@raw_body = response.raw_body
-        @@headers = response.headers
-    end
-
-    def self.checkApiKey(api_key, api_secret)
+  def self.viewDirectDebit(api_key, api_secret, dfc_reference)
       self.clearStoredResponse
 
-      response = Unirest.get @@api_url_checkkey, auth:{:user=>api_key, :password=>api_secret}
+      response = Unirest.get @@api_url_viewdd, auth:{:user=>api_key, :password=>api_secret}, parameters:{ :dfc_reference => dfc_reference }
 
       self.setStoredResponse response
 
       if(response.code === 200 && response.body['status'] === 'ok')
-        return true
+        return response.body
       end
 
 
       return false
+  end
 
-    end
-
-    def self.viewDirectDebit(api_key, api_secret, dfc_reference)
-        self.clearStoredResponse
-
-        response = Unirest.get @@api_url_viewdd, auth:{:user=>api_key, :password=>api_secret}, parameters:{ :dfc_reference => dfc_reference }
-
-        self.setStoredResponse response
-
-        if(response.code === 200 && response.body['status'] === 'ok')
-          return response.body
-        end
-
-
-        return false
-    end
-
-    def self.viewDirectDebitBreakdown(api_key, api_secret, dfc_reference)
-        self.clearStoredResponse
-
-        response = Unirest.get @@api_url_viewddbreakdown, auth:{:user=>api_key, :password=>api_secret}, parameters:{ :dfc_reference => dfc_reference }
-
-        self.setStoredResponse response
-
-        if(response.code === 200 && response.body['status'] === 'ok')
-          return response.body
-        end
-
-
-        return false
-    end
-
-
-    def self.createDirectDebit(api_key, api_secret, client_reference, reference, title, first_name, last_name, address1, address2, address3, town, county, postcode, amounts, email, account_number, sort_code, start_from, installments, frequency_unit, frequency_type, roll_status, birth_date, mobile_number, phone_number, no_email, service_description, bacs_reference, skip_check)
-
+  def self.viewDirectDebitBreakdown(api_key, api_secret, dfc_reference)
       self.clearStoredResponse
 
-      response = Unirest.post @@api_url_setupdd,
-                              headers: {
-                                "Content-Type" => "application/json",
-                                "Accept" => "application/json"
+      response = Unirest.get @@api_url_viewddbreakdown, auth:{:user=>api_key, :password=>api_secret}, parameters:{ :dfc_reference => dfc_reference }
+
+      self.setStoredResponse response
+
+      if(response.code === 200 && response.body['status'] === 'ok')
+        return response.body
+      end
+
+
+      return false
+  end
+
+
+  def self.createDirectDebit(api_key, api_secret, client_reference, reference, title, first_name, last_name, address1, address2, address3, town, county, postcode, amounts, email, account_number, sort_code, start_from, installments, frequency_unit, frequency_type, roll_status, birth_date, mobile_number, phone_number, no_email, service_description, bacs_reference, skip_check)
+
+    self.clearStoredResponse
+
+    response = Unirest.post @@api_url_setupdd,
+                            headers: {
+                              "Content-Type" => "application/json",
+                              "Accept" => "application/json"
+                            },
+                            parameters: {
+                              :authentication => {
+                                :apikey => api_key,
+                                :apisecret => api_secret,
+                                :client_ref => client_reference
                               },
-                              parameters: {
-                                :authentication => {
-                                  :apikey => api_key,
-                                  :apisecret => api_secret,
-                                  :client_ref => client_reference
-                                },
-                                :payer => {
-                                  :title => title,
-                                  :first_name => first_name,
-                                  :last_name => last_name,
-                                  :birth_date => birth_date
-                                },
-                                :address => {
-                                  :address1 => address1,
-                                  :address2 => address2,
-                                  :address3 => address3,
-                                  :town => town,
-                                  :county => county,
-                                  :postcode => postcode,
-                                  :skip_check => skip_check
-                                },
-                                :contact => {
-                                  :phone => phone_number,
-                                  :mobile => mobile_number,
-                                  :email => email,
-                                  :no_email => no_email
-                                },
-                                :bank => {
-                                  :account_number => account_number,
-                                  :sort_code => sort_code
-                                },
-                                :subscription => {
-                                  :reference => reference,
-                                  :description => service_description,
-                                  :amounts => amounts,
-                                  :interval => {
-                                    :unit => frequency_unit,
-                                    :frequency => frequency_type
-                                  },
-                                  :start_from => start_from,
-                                  :installments => installments,
-                                  :bacs_reference => bacs_reference,
-                                  :roll_status => roll_status
-                                }
-                              }.to_json
-
-      self.setStoredResponse response
-
-      if(response.code === 200 && response.body['status'] === 'ok')
-        return true
-      end
-
-      return false
-
-    end
-
-
-    def self.updateDirectDebit(api_key, api_secret, dfc_reference, reference, title, first_name, last_name, address1, address2, address3, town, county, postcode, email, account_number, sort_code, birth_date, mobile_number, phone_number, paymentdate, applyfrom_paydate, installmentduedate, installmentamount, latepayment, applyfrom, newamount)
-      self.clearStoredResponse
-
-      response = Unirest.post @@api_url_updatedd,
-                              headers: {
-                                "Content-Type" => "application/json",
-                                "Accept" => "application/json"
+                              :payer => {
+                                :title => title,
+                                :first_name => first_name,
+                                :last_name => last_name,
+                                :birth_date => birth_date
                               },
-                              parameters: {
-                                :authentication => {
-                                  :apikey => api_key,
-                                  :apisecret => api_secret,
-                                  :dfc_ref => dfc_reference
-                                },
-                                :payer => {
-                                  :title => title,
-                                  :first_name => first_name,
-                                  :last_name => last_name,
-                                  :birth_date => birth_date
-                                },
-                                :address => {
-                                  :address1 => address1,
-                                  :address2 => address2,
-                                  :address3 => address3,
-                                  :town => town,
-                                  :county => county,
-                                  :postcode => postcode
-                                },
-                                :contact => {
-                                  :phone => phone_number,
-                                  :mobile => mobile_number,
-                                  :email => email
-                                },
-                                :bank => {
-                                  :account_number => account_number,
-                                  :sort_code => sort_code
-                                },
-                                :general => {
-                                  :yourref => reference,
-                                  :paymentdate => paymentdate,
-                                  :installmentduedate => installmentduedate,
-                                  :installmentamount => installmentamount,
-                                  :latepayment => latepayment,
-                                  :applyfrom => applyfrom,
-                                  :applyfrom_paydate => applyfrom_paydate,
-                                  :newamount => newamount
-                                }
-                              }.to_json
-
-      self.setStoredResponse response
-
-      if(response.code === 200 && response.body['status'] === 'ok')
-        return true
-      end
-
-      return false
-
-    end
-
-
-    def self.cancelDirectDebit(api_key, api_secret, dfc_reference, cancel_date)
-      self.clearStoredResponse
-
-      response = Unirest.post @@api_url_canceldd,
-                              headers: {
-                                "Content-Type" => "application/json",
-                                "Accept" => "application/json"
+                              :address => {
+                                :address1 => address1,
+                                :address2 => address2,
+                                :address3 => address3,
+                                :town => town,
+                                :county => county,
+                                :postcode => postcode,
+                                :skip_check => skip_check
                               },
-                              parameters: {
-                                :authentication => {
-                                  :apikey => api_key,
-                                  :apisecret => api_secret,
-                                  :dfc_ref => dfc_reference
+                              :contact => {
+                                :phone => phone_number,
+                                :mobile => mobile_number,
+                                :email => email,
+                                :no_email => no_email
+                              },
+                              :bank => {
+                                :account_number => account_number,
+                                :sort_code => sort_code
+                              },
+                              :subscription => {
+                                :reference => reference,
+                                :description => service_description,
+                                :amounts => amounts,
+                                :interval => {
+                                  :unit => frequency_unit,
+                                  :frequency => frequency_type
                                 },
-                                :cancel => {
-                                  :apply_from => cancel_date
-                                }
-                              }.to_json
+                                :start_from => start_from,
+                                :installments => installments,
+                                :bacs_reference => bacs_reference,
+                                :roll_status => roll_status
+                              }
+                            }.to_json
 
-      self.setStoredResponse response
+    self.setStoredResponse response
 
-      if(response.code === 200 && response.body['status'] === 'ok')
-        return true
-      end
-
-      return false
-
+    if(response.code === 200 && response.body['status'] === 'ok')
+      return true
     end
+
+    return false
+
+  end
+
+
+  def self.updateDirectDebit(api_key, api_secret, dfc_reference, reference, title, first_name, last_name, address1, address2, address3, town, county, postcode, email, account_number, sort_code, birth_date, mobile_number, phone_number, paymentdate, applyfrom_paydate, installmentduedate, installmentamount, latepayment, applyfrom, newamount)
+    self.clearStoredResponse
+
+    response = Unirest.post @@api_url_updatedd,
+                            headers: {
+                              "Content-Type" => "application/json",
+                              "Accept" => "application/json"
+                            },
+                            parameters: {
+                              :authentication => {
+                                :apikey => api_key,
+                                :apisecret => api_secret,
+                                :dfc_ref => dfc_reference
+                              },
+                              :payer => {
+                                :title => title,
+                                :first_name => first_name,
+                                :last_name => last_name,
+                                :birth_date => birth_date
+                              },
+                              :address => {
+                                :address1 => address1,
+                                :address2 => address2,
+                                :address3 => address3,
+                                :town => town,
+                                :county => county,
+                                :postcode => postcode
+                              },
+                              :contact => {
+                                :phone => phone_number,
+                                :mobile => mobile_number,
+                                :email => email
+                              },
+                              :bank => {
+                                :account_number => account_number,
+                                :sort_code => sort_code
+                              },
+                              :general => {
+                                :yourref => reference,
+                                :paymentdate => paymentdate,
+                                :installmentduedate => installmentduedate,
+                                :installmentamount => installmentamount,
+                                :latepayment => latepayment,
+                                :applyfrom => applyfrom,
+                                :applyfrom_paydate => applyfrom_paydate,
+                                :newamount => newamount
+                              }
+                            }.to_json
+
+    self.setStoredResponse response
+
+    if(response.code === 200 && response.body['status'] === 'ok')
+      return true
+    end
+
+    return false
+
+  end
+
+
+  def self.cancelDirectDebit(api_key, api_secret, dfc_reference, cancel_date)
+    self.clearStoredResponse
+
+    response = Unirest.post @@api_url_canceldd,
+                            headers: {
+                              "Content-Type" => "application/json",
+                              "Accept" => "application/json"
+                            },
+                            parameters: {
+                              :authentication => {
+                                :apikey => api_key,
+                                :apisecret => api_secret,
+                                :dfc_ref => dfc_reference
+                              },
+                              :cancel => {
+                                :apply_from => cancel_date
+                              }
+                            }.to_json
+
+    self.setStoredResponse response
+
+    if(response.code === 200 && response.body['status'] === 'ok')
+      return true
+    end
+
+    return false
+
   end
 
 end
